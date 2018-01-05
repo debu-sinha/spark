f697da0f3019a6b7b250baabf3b39bd242b427d3
Debu Sinha <debusinha2009@gmail.com>
2018-01-05 16:16:35 -0500

    Update WordCount.scala
    changed path to input file

--- src/main/scala/examples/WordCount.scala
+++ src/main/scala/examples/WordCount.scala
@@ -6,7 +6,7 @@ object WordCount extends App{
   
   val spContext = SparkCommonUtils.spContext
   
-  val file = spContext.textFile("/home/edureka/workspace/projects/spark/src/main/resources/wordcount-sample.tsv")
+  val file = spContext.textFile("<path to wordcount-sample.tsv>")
   
   file.flatMap(r => r.split("\t")).map(r=>(r,1)).reduceByKey((x,y)=>x+y).collect().foreach(println)
   
