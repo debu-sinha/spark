2fc891621f4844979d7398f72e4863a1ae5f4572
Debu Sinha <debusinha2009@gmail.com>
2018-01-05 16:17:05 -0500

    Update WordCount2.scala
    changed path to input file

--- src/main/scala/examples/WordCount2.scala
+++ src/main/scala/examples/WordCount2.scala
@@ -5,7 +5,7 @@ import common.utilities.SparkCommonUtils
 object WordCount2 extends App{
   val spContext = SparkCommonUtils.spContext
   
-  val file = spContext.textFile("/home/edureka/workspace/projects/spark/src/main/resources/wordcount-sample2.txt")
+  val file = spContext.textFile("<path to wordcount-sample2.txt>")
   
   file.map(r=>(r,1)).reduceByKey((x,y)=>x+y).collect().foreach(println)
   
